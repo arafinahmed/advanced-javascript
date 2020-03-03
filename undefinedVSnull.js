99fd175bd966c95efec5f55a4146656b34332f31
arafinahmed <arafin2021@gmail.com>
2020-03-04 01:39:43 +0600

    undefined

---
+++ undefinedVSnull.js
@@ -0,0 +1,13 @@
+let v;
+let add = (x,y) => {
+    let sum = x+y;
+    return sum;
+}
+console.log(add(2,3));
+
+const obj = {name:"name", age:20};
+console.log(obj.p);
+
+let fun = undefined;
+console.log(fun);
+
